2f06c6f1a497ac10567725bd9eccd32a673ab698
AbelJuncal <abel.juncal@udc.es>
2024-02-17 03:01:43 +0100

    Upload models

--- dataset/read_data.py
+++ dataset/read_data.py
@@ -9,14 +9,21 @@ def process_csv(csv_path: str, out_path: str):
     grouped_df = df.groupby('Código universal de punto de suministro')
 
     for group_id, group_df in grouped_df:
-        group_df['Mes_cos'] = np.cos(group_df['Fecha'].dt.month)
-        group_df['Mes_sin'] = np.sin(group_df['Fecha'].dt.month)
-        group_df['Año'] = group_df['Fecha'].dt.year
+        group_df['hora_cos'] = np.cos(group_df['Hora']).round(5)
+        group_df['hora_sin'] = np.sin(group_df['Hora']).round(5)
 
-        group_df['Dia_cos'] = np.cos(group_df['Fecha'].dt.day)
-        group_df['Dia_sin'] = np.sin(group_df['Fecha'].dt.day)
+        group_df['Mes'] = group_df['Fecha'].dt.month
+        group_df['Mes_cos'] = np.cos(group_df['Fecha'].dt.month).round(5)
+        group_df['Mes_sin'] = np.sin(group_df['Fecha'].dt.month).round(5)
+        #group_df['Año'] = group_df['Fecha'].dt.year
+
+        group_df['Dia'] = group_df['Fecha'].dt.day
+        group_df['Dia_cos'] = np.cos(group_df['Fecha'].dt.day).round(5)
+        group_df['Dia_sin'] = np.sin(group_df['Fecha'].dt.day).round(5)
 
         group_df['Dia_week'] = group_df['Fecha'].dt.dayofweek
+        group_df['Dia_week_cos'] = np.cos(group_df['Fecha'].dt.dayofweek).round(5)
+        group_df['Dia_week_sin'] = np.sin(group_df['Fecha'].dt.dayofweek).round(5)
 
         group_df['finde'] = (group_df['Dia_week'] < 5).astype(int)
 
@@ -25,6 +32,8 @@ def process_csv(csv_path: str, out_path: str):
 
         group_df['Estacion'] = group_df['Fecha'].dt.month.map(month_to_season)
 
+        group_df = group_df.drop('Fecha', axis=1)
+
         out_file = f"{out_path}house_{group_id}.csv"
         group_df.to_csv(out_file, index=False)
 
@@ -32,7 +41,7 @@ def process_csv(csv_path: str, out_path: str):
 
 
 if __name__ == '__main__':
-    electrodatos = "../data/electrodatos.csv"
-    out_folder = "/home/agarcia/Escritorio/hackudc/data/"
+    inputFile = "merged.csv"
+    outputFile = "data_"
     # process_csv(electrodatos)
-    process_csv(electrodatos,out_folder)
+    process_csv(inputFile, outputFile)
