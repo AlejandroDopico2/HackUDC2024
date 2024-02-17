315f07b7a1bb402cd1b59f24cff2687da691dd32
AntonCanzobre <anton.c744@gmail.com>
2024-02-17 20:19:41 +0100

    fjkadshjkfha

--- backend/myapi/pdf_utils.py
+++ backend/myapi/pdf_utils.py
@@ -1,22 +1,13 @@
 import os
 import PyPDF2
 
-def read_pdfs(folder_path):
-    files = [file for file in os.listdir(folder_path) if file.endswith(".pdf")]
-    info_files = []
 
-    for file in files:
-        text = read_pdf(folder_path+file)
-        info = extract_information(text)
-        info_files.append(f"{file} information:\n{info}")
-
-    return " ".join(info_files)
 def read_pdf(path):
     pdf = open(path, 'rb')
     pdfReader = PyPDF2.PdfReader(pdf)
     pageObj = pdfReader.pages[0]
-
-    return pageObj.extract_text()
+    res = extract_information(pageObj)
+    return res
 
 def extract_information(text):
     nombre_empresa = text.split("-")[0]
@@ -39,8 +30,3 @@ def extract_information(text):
 
     return (f"Nombre de la Empresa: {nombre_empresa}\nNúmero de Factura:{numero_factura}\nFecha de Emisión:{fecha_emision}\nFecha de Cargo:{fecha_cargo}\nTotal a Pagar:{total_pagar} Dirección de Suministro:{direccion_suministro}\n"
             f"Detalle de Costos:\n Energía: {detalle_costos[0]} %\n Alquiler contador: {detalle_costos[1]} %\n Impuestos: {detalle_costos[2]} %\n Cargos: {detalle_costos[3]}\n Peajes: {detalle_costos[4]} %\n\n\n")
-
-
-if __name__ == '__main__':
-    info = read_pdfs("/home/agarcia/Escritorio/HackUDC2024-main/pdfs/")
-    print(info)
